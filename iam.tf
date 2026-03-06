30088d876cc459fa5e98c8b2267748f99939cc13
fal <faljirushi@gmail.com>
2026-03-06 20:30:29 +0900

    ec2用のiamロールを作成

---
+++ iam.tf
@@ -0,0 +1,44 @@
+#-----------------
+#IAM Role
+#-----------------
+
+resource "aws_iam_instance_profile" "app_ec2_profile" {
+  name = aws_iam_role.app_iam_role.name
+  role = aws_iam_role.app_iam_role.name
+}
+
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${var.project}-${var.enviroment}-app-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.app_iam_role.name                    #関連付けるIAMロール
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess" #公式のArn
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
