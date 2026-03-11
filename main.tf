fc57e1507b2aad787d6fd7d211ef034a1f48e5f9
fal <faljirushi@gmail.com>
2026-03-11 23:17:07 +0900

    Route53を追加

--- main.tf
+++ main.tf
@@ -37,6 +37,11 @@ variable "enviroment" {
   type = string
 }
 
+variable "domain" {
+  type = string
+
+}
+
 #rdsの起動に時間がかかるため以下を追加
 # terraform apply -var="create_rds=true"で指定しない場合はrdsは起動されない。
 variable "create_rds" {
