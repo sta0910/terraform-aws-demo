970305369ee4bd5c8cf3d6a2cc7a9db1d04e0798
fal <faljirushi@gmail.com>
2026-03-13 16:02:46 +0900

    s3を追加

---
+++ s3.tf
@@ -0,0 +1,94 @@
+resource "random_string" "s3_unique_key" {
+  length  = 8
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
+#--------------------
+# Static bucket
+#--------------------
+
+resource "aws_s3_bucket" "s3_static_bucket" {
+  bucket = "${var.project}-${var.enviroment}-static-bucket${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_versioning" "s3_static_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_static_bucket" {
+  bucket                  = aws_s3_bucket.s3_static_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+  depends_on = [
+    aws_s3_bucket_policy.s3_static_bucket
+  ]
+}
+
+resource "aws_s3_bucket_policy" "s3_static_bucket" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+  policy = data.aws_iam_policy_document.s3_static_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_static_bucket" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+#--------------------
+# s3 deploy bucket
+#--------------------
+
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "${var.project}-${var.enviroment}-deploy-bucket${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_versioning" "s3_deploy_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket                  = aws_s3_bucket.s3_deploy_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on = [
+    aws_s3_bucket_policy.s3_deploy_bucket
+  ]
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.s3_deploy_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_deploy_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.app_iam_role.arn]
+    }
+  }
+}
+
